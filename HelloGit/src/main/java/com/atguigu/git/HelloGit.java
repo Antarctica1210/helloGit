e7721788074beae2187bf9a4c2e42d8593355b0e
Antarctica1210 <wangyufan940223@gmail.com>
2021-02-09 18:15:10 +1300

    add files

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author Antarctica1210
+ * @description
+ * @create 2021-02-09-16:22
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
